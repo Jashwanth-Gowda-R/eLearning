0e81ad8a62650fcd43b46e72c4cd5c2ae773b5ba
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-08-21 16:23:25 +0530

    created ui for login screen

---
+++ lib/common/widgets/button_widgets.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:vrook_course/common/values/values.dart';
+
+Widget appButton({
+  required Color color,
+  required Function() onTap,
+  required String title,
+  required Color textcolor,
+  double width = 325,
+  double height = 50,
+}) {
+  return GestureDetector(
+    onTap: onTap,
+    child: Container(
+      width: width,
+      height: height,
+      decoration: appBoxShadow(color: color).copyWith(
+          border: Border.all(color: AppColors.primaryFourElementText)),
+      child: Center(
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: textcolor,
+            fontWeight: FontWeight.normal,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    ),
+  );
+}
--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vrook_course/common/values/values.dart';
+import 'package:vrook_course/common/widgets/button_widgets.dart';
 import 'package:vrook_course/common/widgets/text_widgets.dart';
 import 'package:vrook_course/pages/sign_in/widgets/sign_in_widgets.dart';
 
@@ -12,44 +13,65 @@ class SignIn extends StatelessWidget {
       backgroundColor: AppColors.primaryBackground,
       appBar: buildAppBar(),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            thirdPartyLogin(),
-            Center(
-              child: text14Normal(text: 'Or use your email account login'),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            appTextField(
-              title: 'Email',
-              hintText: "Enter your email address",
-              imgPath: 'assets/icons/user.png',
-              onChanged: (value) {},
-              obsText: false,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            appTextField(
-              title: 'Password',
-              hintText: "Enter your Password",
-              imgPath: 'assets/icons/lock.png',
-              onChanged: (value) {},
-              obsText: true,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 25),
-              child: textUnderline(text: 'Forgot Password?', onTap: () {}),
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              thirdPartyLogin(),
+              Center(
+                child: text14Normal(text: 'Or use your email account login'),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              appTextField(
+                title: 'Email',
+                hintText: "Enter your email address",
+                imgPath: 'assets/icons/user.png',
+                onChanged: (value) {},
+                obsText: false,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              appTextField(
+                title: 'Password',
+                hintText: "Enter your Password",
+                imgPath: 'assets/icons/lock.png',
+                onChanged: (value) {},
+                obsText: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 25),
+                child: textUnderline(text: 'Forgot Password?', onTap: () {}),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Center(
+                child: appButton(
+                  color: AppColors.primaryElement,
+                  onTap: () {},
+                  title: 'Log In',
+                  textcolor: AppColors.primaryBackground,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: appButton(
+                  color: AppColors.primaryBackground,
+                  onTap: () {},
+                  title: 'Sign Up',
+                  textcolor: AppColors.primaryText,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
