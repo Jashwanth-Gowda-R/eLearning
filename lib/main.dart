910eeb1825748987d30ed59a2dd93de45e341538
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-08-24 20:19:31 +0530

    added firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vrook_course/common/style/theme.dart';
+import 'package:vrook_course/firebase_options.dart';
 import 'package:vrook_course/pages/sign_in/sign_in.dart';
 import 'package:vrook_course/pages/sign_up/sign_up.dart';
 import 'package:vrook_course/pages/welcome/welcome.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
